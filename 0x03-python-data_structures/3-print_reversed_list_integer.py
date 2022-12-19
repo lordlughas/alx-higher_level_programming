db4678c8b290b8a45c93ab2e342acf82f4f7b9ca
Charles Lughas <lordlughas@gmail.com>
2022-12-19 06:19:03 -0800

    Hot fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-# 3-print_reversed_list_integer.py
-=======
-#print_reversed_list_integer.py
->>>>>>> 6298629b205210e7dae24b1ec46807aeeea58dc5
-
 
 def print_reversed_list_integer(my_list=[]):
     """
