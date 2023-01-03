87942a9316defa1ddd6f10340ee3872b6c941241
Charles Lughas <lordlughas@gmail.com>
2023-01-03 14:37:31 -0800

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """
+    adds all unique integers in a list (only once for each integer)
+    """
+    output = 0
+    for idx in set(my_list):
+        output += idx
+    return output
+
