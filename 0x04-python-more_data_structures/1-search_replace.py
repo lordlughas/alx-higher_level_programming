ad6743d2b6f9fe7bf72caaf869a1090b292a6910
Charles Lughas <lordlughas@gmail.com>
2023-01-03 14:30:49 -0800

    Added task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+    search and replace word, string or integer
+    """
+new_list = my_list[:]
+    for idx in range(len(update_list)):
+        if new_list[idx] == search:
+            new_list[idx] = replace
+    return (new_list)
