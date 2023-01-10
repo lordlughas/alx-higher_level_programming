835d52ae37327227d765cd63cd7bb959b70a777b
Charles Lughas <lordlughas@gmail.com>
2023-01-10 07:20:02 -0800

    Updated task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
+def number_keys(a_dictionary):
     """
-    Return the number of keys in a dictionary.
+    Return the number of keys in a dictionary
     """
     return (len(a_dictionary))
