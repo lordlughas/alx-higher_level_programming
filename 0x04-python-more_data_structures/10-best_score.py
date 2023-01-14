bc1c2ad02754eb6f88ac8a6973aa5bf11237a3a3
Charles Lughas <lordlughas@gmail.com>
2023-01-13 16:25:51 -0800

    fixed code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,10 +5,10 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
- res = list(a_dictionary.keys())[0]
-    big = a_dictionary[res]
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
     for k, v in a_dictionary.items():
         if v > big:
             big = v
-            res = k
-    return (res)
+            ret = k
+    return (ret)
--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,8 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-score = 0
+
+    score = 0
     weight = 0
     for x, y in my_list:
         score += (x * y)
