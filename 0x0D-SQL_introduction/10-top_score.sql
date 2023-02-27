29dad6d892fd7ea340f01d3f978e383c7749323b
Charles Lughas <lordlughas@gmail.com>
2023-02-27 06:09:58 -0800

    Hot fix script

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database hntn_0c_0 in mysql server
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC, name;
