dbdeec2b8487aaac9d98e2192e3091c622b276ce
Charles Lughas <lordlughas@gmail.com>
2023-02-27 04:41:46 -0800

    Add sql scripts

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- a script that lists all databases of my MySQL servr
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script that deletes the databases hbtn_0c_0 in MySQL server
+DROP DATABASE IF EXITS hbtn_0c_0
