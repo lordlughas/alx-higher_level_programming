b804ab46dbd73691d8ece87ef4e327bd79a63f2f
Charles Lughas <lordlughas@gmail.com>
2023-03-14 02:10:52 -0700

    fix on task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurences');
 } else {
-  const x = Number(process.argv[2]);
+  const x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {
   console.log('C is fun')};
 }
