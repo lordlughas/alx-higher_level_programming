006b7616f3075e0d8e3cd512834fdb5b4b25cede
Charles Lughas <lordlughas@gmail.com>
2023-03-14 04:37:49 -0700

    Add tasks scripts

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2] || process.argv[2] === undefined) {
+if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
   console.log('Missing size');
 } else {
   const x = Number(process.argv[2]);
   for (let i = 0; i < x; i++) {
-  console.log(X ** x);
+    console.log('X'.repeat(x));
   }
 }
