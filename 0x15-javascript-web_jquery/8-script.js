012cff417717b51d40c6a7841bc7d839aadf67db
Charles Lughas <lordlughas@gmail.com>
2023-05-17 17:30:57 +0100

    Add task 1-9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,4 @@
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+    
+});
